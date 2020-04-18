22c6aac024d35e813c234ef59784348091973e08
Hein Andre Grønnestad <hag@haugnett.no>
2020-04-18 10:52:33 +0200

    Added a health system interface and did some organizing

---
+++ LD46Game/Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies {
+    class EnemyBase : MonoBehaviour, IHealthSystem {
+
+        private float _health;
+
+        float IHealthSystem.Health { get => _health; set => _health = value; }
+
+        public void Damage(float amount) {
+            _health -= amount;
+            if (_health < 0f) _health = 0f;
+
+            if (_health == 0) Kill();
+        }
+
+        public void Heal(float amount) {
+            _health += amount;
+            if (_health > 1.0f) _health = 1.0f;
+        }
+
+        public void Kill() {
+            Destroy(this);
+        }
+    }
+}
---
+++ LD46Game/Assets/Scripts/Interfaces/IHealthSystem.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Interfaces {
+    public interface IHealthSystem {
+        float Health { get; set; }
+        void Damage(float amount);
+        void Heal(float amount);
+    }
+}
