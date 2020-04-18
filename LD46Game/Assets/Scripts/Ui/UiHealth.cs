6588c4dbbdc8b32404fa33e4d0ca09322f01358b
Hein Andre Grønnestad <hag@haugnett.no>
2020-04-18 14:57:10 +0200

    UI heart health bar

---
+++ LD46Game/Assets/Scripts/Ui/UiHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Ui {
+    public class UiHealth : MonoBehaviour {
+
+        public float Health;
+
+        public Image frame;
+        public Image fill;
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            fill.rectTransform.sizeDelta = new Vector2(frame.rectTransform.rect.width, frame.rectTransform.rect.height * Health);
+        }
+    }
+}
