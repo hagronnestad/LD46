9b3fb0d83f89eca10d4a10cef938a152a6ec1b1a
Gary <garytmulhall@gmail.com>
2020-04-18 17:14:12 +0100

    Revert "Camera work"
    This reverts commit f8d501b336fa0192774e5862ca8eb88da2654844.

--- LD46Game/Assets/Scripts/Enemies/SimpleEnemy.cs
+++ LD46Game/Assets/Scripts/Enemies/SimpleEnemy.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 namespace Assets.Scripts.Enemies {
     public class SimpleEnemy : EnemyBase {
-        [SerializeField] Transform rayCastPoint;
-
         Vector2 movement;
         public float speed = 1f;
-        public float sightRange;
         public Transform player;
-        public bool agro;
         Rigidbody2D enemyRigidBody;
 
         void Start() {
@@ -24,39 +20,11 @@ namespace Assets.Scripts.Enemies {
         }
 
         void FixedUpdate() {
-            if (agro) {
-                Move(movement);
-            }
+            Move(movement);
         }
 
         void Move(Vector2 direction) {
             enemyRigidBody.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
-        }
-
-        void OnTriggerEnter2D(Collider2D collision) {
-            if(collision.gameObject.tag == "Player") {
-                agro = true;
-            }
-        }
-        void OnCollisionEnter2D(Collision2D collision) {
-            if (collision.gameObject.tag == "Player") {
-                Damage(0.2f);
-            }
-        }
-        bool HasSight(float distance) {
-            bool sightValue = false;
-            float rayDistance = distance;
-            Vector2 rayEndPoint = rayCastPoint.position + Vector3.right * distance;
-            RaycastHit2D hit = Physics2D.Linecast(rayCastPoint.position, rayEndPoint, 1 << LayerMask.NameToLayer("Action"));
-            Debug.DrawLine(rayCastPoint.position, hit.point, Color.red);
-            if(hit.collider != null) {
-                if(hit.collider.gameObject.CompareTag("Player")) {
-                    sightValue = true;
-                } else {
-                    sightValue = false;
-                }
-            }
-            return sightValue;
         }
     }
 }
