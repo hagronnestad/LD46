c2163fee48f30d18bd161982b1d610cf96845030
Hein Andre Grønnestad <hag@haugnett.no>
2020-04-19 16:00:29 +0200

    Simple audio fx manager

---
+++ LD46Game/Assets/Scripts/Audio/AudioClipWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Audio {
+
+    [Serializable]
+    public class AudioClipWrapper {
+
+        public string Name;
+        public AudioClip AudioClip;
+
+    }
+
+}
---
+++ LD46Game/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Audio {
+
+    public class AudioManager : MonoBehaviour {
+
+        public static AudioManager Instance;
+
+        public List<AudioClipWrapper> AudioClips;
+        public AudioSource AudioSource;
+
+        private void Awake() {
+            if (Instance == null) Instance = this;
+        }
+
+        public void Play(string name) {
+            var audio = AudioClips.FirstOrDefault(x => x.Name == name);
+
+            if (audio == null) return;
+
+            Play(audio.AudioClip);
+        }
+
+        public void Play(AudioClip audioClip) {
+            AudioSource.PlayOneShot(audioClip);
+        }
+
+    }
+
+}
