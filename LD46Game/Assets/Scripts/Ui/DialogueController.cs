71290743facd6fe4014ae640adfdd3bb8b6b3948
Serena <heyruhs@gmail.com>
2020-04-20 20:03:47 +0100

    Made menu and story scene

---
+++ LD46Game/Assets/Scripts/Ui/DialogueController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+    IEnumerator ShowText() 
+    {
+        for (int i= 0; i < fullText.Length; i++) 
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+}
